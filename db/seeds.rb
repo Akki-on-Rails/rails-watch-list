2dc3fe50d654024fc2aec7718bc8388824262d5a
Frank Gerngross <f.gerngross@googlemail.com>
2022-07-16 17:16:33 +0200

    created routes and controllers for list and bookmark

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'net/http'
+require 'uri'
+
+puts "Cleaning DB"
+Movie.destroy_all
+
+uri = URI('https://tmdb.lewagon.com/movie/top_rated')
+response = Net::HTTP.get(uri)
+movie_list = JSON.parse(response)
+
+puts "Creating Movies..."
+index = 0
+10.times do
+  Movie.create!(
+    title: movie_list["results"][index]["title"],
+    overview: movie_list["results"][index]["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie_list["results"][index]["poster_path"]}",
+    rating: movie_list["results"][index]["vote_average"]
+  )
+  puts "Movie #{index}..."
+  index += 1
+end
+
+puts "Done"
